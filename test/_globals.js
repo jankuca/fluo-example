84ff166e224c25c0d41c5dc35b84b93199c5461e
jankuca <jan@jankuca.com>
2015-02-23 10:00:21 +0100

    add node.js testing (no browser needed)

--- test/_globals.js
+++ test/_globals.js
@@ -1,2 +1,30 @@
+var jsdom = require('jsdom');
+var expect = require('expect.js');
 
-global.expect = require('expect.js');
+
+beforeEach(function (callback) {
+  global.expect = expect;
+
+  if (typeof global.window !== 'undefined') {
+    return callback();
+  }
+
+  global.navigator = {
+    userAgent: 'node.js'
+  };
+
+  jsdom.env('', [], function (errs, window) {
+    global.window = window;
+    global.document = window.document;
+
+    callback();
+  });
+});
+
+
+afterEach(function () {
+  delete global.document;
+  delete global.window;
+  delete global.navigator;
+  delete global.expect;
+});
